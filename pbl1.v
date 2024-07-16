1e1a8d5c3249e1dfee2c540aefa2d4c69222d0f0
Gabriel Ribeiro <109034356+grsouzaecomp@users.noreply.github.com>
2024-07-16 20:51:38 -0300

    PBL 1 Atualizado

--- pbl1.v
+++ pbl1.v
@@ -1,53 +1,40 @@
-// Declaração do Módulo
-module pbl1(H, M, L, Bs, Vs, Ve, Us, Ua, T, Al, E, working, clock, column, lines, segA, segB, segC, segD, segE, segF, segG, seven_seg_digit);
-	// Declaração das Portas
-	input H, M, L, T, Us, Ua, clock;
-	output Bs, Vs, Ve, Al, E, working, segA, segB, segC, segD, segE, segF, segG;
-	output [3:0] seven_seg_digit;
-	output [4:0] column;
-	output [6:0] lines;
-	
-	// Declaração dos Fios
-	wire w0, w1, notL, notM, w4, w5, w6, w7, lowUs, lowUa, w10, w11, lowTemp, w13, w14, w15, w16, w17, notErro, notH, w20, notAlarme;
-	
-	// Circuito de Erro de Medição
-	not Not0 (notL, L);
-	not Not1 (notM, M);
-	and And0 (w0, M, notL);
-	and And1 (w1, H, notM);
-	or Or0 (E, w0, w1);
-	not Not2 (notErro, E); // Inversor do Erro para Fechar os Acionamentos
-	
-	// Circuito de Acionamento da Válvula de Entrada
-	not Not3 (notH, H);
-	and And2 (w4, notH, notM, notL, notErro);
-	and And3 (w5, notH, L, notErro);
-	or Or1 (Ve, w4, w5);
-	
-	// Circuito de Acionamento do Alarme
-	and And4 (w6, notH, notM, L); // Nível mínimo
-	and And5 (w7, notH, notM, notL); // Nível crítico
-	or Or2 (Al, w6, w7, E); // Nível mínimo, crítico ou erro de medição
-	
-	// Circuito de Acionamento do Sistema de Aspersão
-	not Not4 (lowUs, Us);
-	not Not5 (lowUa, Ua);
-	and And7 (w10, lowUs, lowUa);
-	and And8 (w11, lowUs, Ua, T, M);
-	or Or4 (w16, w10, w11);
-	and And9 (Bs, w16, notErro); // Fechar o Acionamento em Caso de Erro
-	
-	// Circuito de Acionamento do Sistema de Gotejamento
-	not Not6 (lowTemp, T);
-	and And10 (w13, lowUs, Ua, lowTemp);
-	and And11 (w14, lowUs, Ua, T, notM, L);
-	or Or5 (w17, w13, w14);
-	and And12 (Vs, w17, notErro); // Fechar o Acionamento em Caso de Erro
-	
-	// Acionamento dos LEDs de status de funcionamento
-	assign working = notErro; // LED verde que indica o funcionamento correto
-	
-	// Instancionando o Módulo que Implementa o PBL 2 e sua Melhorias:
-	pbl2 part2(.H(H), .M(M), .L(L), .Bs(Bs), .Vs(Vs), .clock(clock), .column(column), .lines(lines), .A(segA),
-	.B(segB), .C(segC), .D(segD), .E(segE), .F(segF), .G(segG), .seven_seg_digit(seven_seg_digit), .Error(E));
+// MÓDULO DO CIRCUITO COMBINACIONAL:
+module combinacional (
+  input H, M, L, Ua, Us, T, 
+  output Ve, Bs, Vs, Al, E;
+);
+
+  // FIOS INTERMEDIÁRIOS:
+  wire W0, W1, W2, W3, W4, W5, W4, W6, W7, W8, W9, W10, W11, W12, W13;
+
+  // VÁLVULA DE ENTRADA:
+  nor nor0 (W8, Al, H);
+  nor nor1 (W9, H, M, L);
+  or or0 (Ve, W8, W9);
+
+  //ASPERSÃO:
+  nor nor2 (Bs, Us, Vs, Al);
+
+  // GOTEJAMENTO:
+  nand nand0 (W3, Ua, T);
+  nor nor3 (W4, W3, Us);
+  nor nor4 (W5, Us, T, M);
+  and and0 (W4, W5, Ua);
+  or or1 (W6, W4, W4);
+  not not0 (W7, Al);
+  and and1 (Vs, W6, W7);
+
+  // ALARME:
+  nand nand1 (W0, M, L);
+  and and1 (W2, W0, H);
+  nor nor5 (W1, H, L);
+  or or2 (Al, W1, W2, E);
+
+  // ERRO:
+  not not1 (W10, L);
+  and and2 (W11, W10, M);
+  not not2 (W12, M);
+  and and3 (W13, W12, H);
+  or or3 (E, W11, W13);
+
 endmodule
