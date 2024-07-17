7957bf1c14202c5103f2c610b89c6689f7293bc4
Gabriel Ribeiro <109034356+grsouzaecomp@users.noreply.github.com>
2024-07-16 21:40:12 -0300

    Add files via upload

--- pbl1.v
+++ pbl1.v
@@ -1,46 +1,39 @@
 module pbl1 (
-	input H, M, L, T, Us, Ua, clock,
-	output Bs, Vs, Ve, Al, E, segA, segB, segC, segD, segE, segF, segG,
-	output [3:0] seven_seg_digit,
-	output [4:0] column,
-	output [6:0] lines
+  input H, M, L, Ua, Us, T, 
+  output Ve, Bs, Vs, Al, E
 );
 
   // FIOS INTERMEDIÁRIOS:
-  wire W0, W1, W2, W3, W4, W5, W4b, W6, W7, W8, W9, W10, W11, W12, W13;
-
-  // VÁLVULA DE ENTRADA:
-  nor nor0 (W8, Al, H);
-  nor nor1 (W9, H, M, L);
-  or or0 (Ve, W8, W9);
-
-  //ASPERSÃO:
-  nor nor2 (Bs, Us, Vs, Al);
-
-  // GOTEJAMENTO:
-  nand nand0 (W3, Ua, T);
-  nor nor3 (W4, W3, Us);
-  nor nor4 (W5, Us, T, M);
-  and and0 (W4b, W5, Ua);
-  or or1 (W6, W4, W4b);
-  not not0 (W7, Al);
-  and and1 (Vs, W6, W7);
-
-  // ALARME:
-  nand nand1 (W0, M, L);
-  and and2 (W2, W0, H);
-  nor nor5 (W1, H, L);
-  or or2 (Al, W1, W2, E);
+  wire W0, W1, W2, W3, W4, W5, W6, W7, W8, W9, W10, W11, W12, W13, W14;
+	
+	// VÁLVULA DE ENTRADA:
+	nor nor0 (W0, Al, H);
+	nor nor1 (W1, H, M, L);
+	or or0 (Ve, W0, W1);
 
   // ERRO:
-  not not1 (W10, L);
-  and and3 (W11, W10, M);
-  not not2 (W12, M);
-  and and4 (W13, W12, H);
-  or or3 (E, W11, W13);
-  
-  // Instancionando o Módulo que Implementa o PBL 2 e sua Melhorias:
-	pbl2 part2(.H(H), .M(M), .L(L), .Bs(Bs), .Vs(Vs), .clock(clock), .column(column), .lines(lines), .A(segA),
-	.B(segB), .C(segC), .D(segD), .E(segE), .F(segF), .G(segG), .seven_seg_digit(seven_seg_digit), .Error(E));
+  not not0 (W2, L);
+  and and0 (W3, W2, M);
+  not not1 (W4, M);
+  and and1 (W5, W4, H);
+  or or1 (E, W3, W5);
+	
+	// ALARME:
+	nand nand0 (W6, M, L);
+	and and2 (W7, W6, H);
+	nor nor2 (W8, H, L);
+	or or2 (Al, W8, W7);
+
+  //ASPERSÃO:
+  nor nor3 (Bs, Us, Vs, Al);
+
+  // GOTEJAMENTO:
+  nand nand1 (W9, Ua, T);
+	nor nor4 (W10, W9, Us);
+	nor nor5 (W11, Us, T, M);
+	and and3 (W12, W11, Ua);
+	or or3 (W13, W10, W12);
+	not not2 (W14, Al);
+	and and4 (Vs, W13, W14);
 
 endmodule
